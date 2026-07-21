e7882b5f3fd161ce62583b10028c63cd102088d8
Metaimperiya <ddos.atack.service@gmail.com>
2026-07-22 01:16:56 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Раздаём статику из папки public
+app.use(express.static('public'));
+
+const players = {};
+
+wss.on('connection', (ws) => {
+  const id = Date.now().toString(36) + Math.random().toString(36).substr(2, 4);
+  
+  players[id] = {
+    x: (Math.random() - 0.5) * 10,
+    z: (Math.random() - 0.5) * 10,
+    y: 0,
+    color: Math.floor(Math.random() * 0xffffff),
+    name: 'Игрок_' + Math.random().toString(36).substr(2, 4)
+  };
+
+  console.log(`🟢 Игрок ${id} подключился (${Object.keys(players).length} всего)`);
+
+  ws.send(JSON.stringify({
+    type: 'init',
+    players: players,
+    myId: id
+  }));
+
+  broadcast({
+    type: 'playerJoin',
+    id: id,
+    x: players[id].x,
+    z: players[id].z,
+    color: players[id].color,
+    name: players[id].name
+  }, ws);
+
+  ws.on('message', (message) => {
+    try {
+      const data = JSON.parse(message);
+      if (data.type === 'move' && players[id]) {
+        players[id].x = data.x;
+        players[id].z = data.z;
+        players[id].rotation = data.rotation || 0;
+        broadcast({
+          type: 'playerMove',
+          id: id,
+          x: data.x,
+          z: data.z,
+          rotation: data.rotation || 0
+        }, ws);
+      }
+      if (data.type === 'chat') {
+        broadcast({
+          type: 'chat',
+          id: id,
+          text: data.text,
+          name: players[id]?.name || 'Игрок'
+        }, ws);
+      }
+    } catch (e) {}
+  });
+
+  ws.on('close', () => {
+    console.log(`🔴 Игрок ${id} отключился`);
+    delete players[id];
+    broadcast({ type: 'playerLeave', id: id });
+  });
+});
+
+function broadcast(data, exclude) {
+  wss.clients.forEach((client) => {
+    if (client !== exclude && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`🚀 Сервер запущен на порту ${PORT}`);
+});
