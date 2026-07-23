0dffe40886de6d20cfa640af2745d4a0d2c4583b
Metaimperiya <ddos.atack.service@gmail.com>
2026-07-23 05:26:29 +0300

    Update world.js

--- public/src/core/world.js
+++ public/src/core/world.js
@@ -2,7 +2,7 @@
 // МИР (МОРЕ)
 // ============================================================
 
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+import * as THREE from 'three';
 import { scene } from './scene.js';
 
 export function createWorld() {
